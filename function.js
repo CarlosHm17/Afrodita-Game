9405119f81dea39eacdb5712e1da842f44bbb8be
CarlosHm17 <candreshm03@gmail.com>
2023-01-09 22:39:18 -0500

    Juego terminado 0.1

--- function.js
+++ function.js
@@ -9,3 +9,19 @@ function resultadoUno() {
     botonD.classList.add("botonS");
     botonT.classList.add("botonT");
 }
+
+botonD.addEventListener("click", resultadoDos);
+
+function resultadoDos() {
+    botonU.classList.add("botonS");
+    botonD.classList.add("botonP");
+    botonT.classList.add("botonT");
+}
+
+botonT.addEventListener("click", resultadoTres);
+
+function resultadoTres() {
+    botonU.classList.add("botonP");
+    botonD.classList.add("botonS");
+    botonT.classList.add("botonT");
+}
